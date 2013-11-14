43c6ae27df297cbc78d7cb8c1f7fa201050ab17f
jacky li <veryfaint@gmail.com>
2013-11-14 18:38:10 +1100

    show "bang" effect after each collision

--- UIDynamicExample/Views/BasicCollisionView.m
+++ UIDynamicExample/Views/BasicCollisionView.m
@@ -1,13 +1,14 @@
 #import "BasicCollisionView.h"
 
 @implementation BasicCollisionView {
-    BOOL collisionEnded;
+    UIImageView *_imageView;
 }
 
 - (id)init {
     self = [super init];
     if (self) {
         [self addDynamicBehaviour];
+        [self createBangImage];
     }
     return self;
 }
@@ -20,22 +21,32 @@
     UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[ballView]];
     [collisionBehavior setTranslatesReferenceBoundsIntoBoundary:YES];
     [theAnimator addBehavior:collisionBehavior];
+
+    UIDynamicItemBehavior *itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[ballView]];
+    [itemBehavior setElasticity:0.6];
+    [theAnimator addBehavior:itemBehavior];
     [collisionBehavior setCollisionDelegate:self];
 }
 
-- (void)collisionBehavior:(UICollisionBehavior *)behavior endedContactForItem:(id <UIDynamicItem>)item withBoundaryIdentifier:(id <NSCopying>)identifier {
-    if (collisionEnded) {
-        return;
-    }
-    collisionEnded = true;
+- (void)createBangImage {
+    _imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bang.png"]];
+    [_imageView setFrame:CGRectMake(100, [UIScreen mainScreen].bounds.size.height - 100, 40, 40)];
+    [_imageView.layer setHidden:YES];
+    [self addSubview:_imageView];
+}
 
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
-    animation.fromValue = [NSValue valueWithCGPoint:ballView.layer.position];
-    animation.toValue = [NSValue valueWithCGPoint:CGPointMake([UIScreen mainScreen].bounds.size.width - ballView.frame.size.width / 2, [UIScreen mainScreen].bounds.size.height - ballView.frame.size.height / 2)];
-    animation.duration = 1;
-    [animation setRemovedOnCompletion:NO];
-    animation.fillMode = kCAFillModeForwards;
-    [ballView.layer addAnimation:animation forKey:nil];
+- (void)collisionBehavior:(UICollisionBehavior *)behavior
+      endedContactForItem:(id <UIDynamicItem>)item
+   withBoundaryIdentifier:(id <NSCopying>)identifier {
+    [self bang];
+}
+
+- (void)bang {
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"hidden"];
+    [animation setToValue:[NSNumber numberWithBool:NO]];
+    [animation setDuration:0.2];
+    [animation setRemovedOnCompletion:YES];
+    [_imageView.layer addAnimation:animation forKey:nil];
 }
 
 
