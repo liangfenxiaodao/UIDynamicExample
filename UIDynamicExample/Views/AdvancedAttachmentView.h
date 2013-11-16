6313280ba90d301292a6eed625a96bce6d0f5069
jacky li <veryfaint@gmail.com>
2013-11-16 22:16:20 +1100

    adjust the advanced attachment behaviour

--- UIDynamicExample/Views/AdvancedAttachmentView.h
+++ UIDynamicExample/Views/AdvancedAttachmentView.h
@@ -1,4 +1,4 @@
 #import <Foundation/Foundation.h>
 
-@interface AdvancedAttachmentView : UIView
+@interface AdvancedAttachmentView : UIView <UICollisionBehaviorDelegate>
 @end
--- UIDynamicExample/Views/AdvancedAttachmentView.m
+++ UIDynamicExample/Views/AdvancedAttachmentView.m
@@ -5,15 +5,22 @@
     UIDynamicAnimator *animator;
     UIAttachmentBehavior *dragBehaviour;
     NSMutableArray *ballViews;
+    UIPanGestureRecognizer *panGestureRecognizer;
 }
 - (id)init {
     self = [super init];
     if (self) {
         [self buildBall];
+        [self setupAnimatorAndGestureRecognizer];
         [self addBehaviour];
     }
 
     return self;
+}
+
+- (void)setupAnimatorAndGestureRecognizer {
+    animator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
+    panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleAttachmentGesture:)];
 }
 
 - (void)buildBall {
@@ -27,13 +34,11 @@
 }
 
 - (void)addBehaviour {
-    animator = [[UIDynamicAnimator alloc] initWithReferenceView:self];
-
     for (NSUInteger i = 0; i < [ballViews count] - 1; i++) {
         UIAttachmentBehavior *attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:ballViews[i] attachedToItem:ballViews[i + 1]];
         [animator addBehavior:attachmentBehavior];
     }
-    [self addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleAttachmentGesture:)]];
+    [self addGestureRecognizer:panGestureRecognizer];
 }
 
 - (void)handleAttachmentGesture:(UIPanGestureRecognizer *)gestureRecognizer {
@@ -44,6 +49,7 @@
         [dragBehaviour setAnchorPoint:[gestureRecognizer locationInView:self]];
     } else if (gestureRecognizer.state == UIGestureRecognizerStateEnded) {
         [animator removeAllBehaviors];
+        [self removeGestureRecognizer:panGestureRecognizer];
         [self addGravityAfterDragEnded];
     }
 }
@@ -54,6 +60,7 @@
 
     UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:ballViews];
     [collisionBehavior setTranslatesReferenceBoundsIntoBoundary:YES];
+    [collisionBehavior setCollisionDelegate:self];
     [animator addBehavior:collisionBehavior];
 }
 
@@ -61,11 +68,15 @@
     UIView *ballView = ballViews[0];
     dragBehaviour = [[UIAttachmentBehavior alloc] initWithItem:ballView attachedToAnchor:anchorPosition];
     double length = [self getDistanceBetweenAnchor:anchorPosition andBallView:ballView];
-    [dragBehaviour setLength:(CGFloat) length];
+    [dragBehaviour setLength:((CGFloat) length  < 20) ? (CGFloat) length : 20];
 }
 
 - (double)getDistanceBetweenAnchor:(CGPoint)anchor andBallView:(UIView *)ballView {
     return sqrt(pow((anchor.x - ballView.center.x), 2.0) + pow((anchor.y - ballView.center.y), 2.0));
+}
+
+- (void)collisionBehavior:(UICollisionBehavior *)behavior endedContactForItem:(id <UIDynamicItem>)item withBoundaryIdentifier:(id <NSCopying>)identifier {
+    [self addBehaviour];
 }
 
 
